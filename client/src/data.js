00c523def4bc4553367e5d44d5fcaeb74042cba1
Jabril-Jeylani <jabrilmj@gmail.com>
2023-08-31 21:37:36 -0600

    everything got messed up

---
+++ client/src/data.js
@@ -0,0 +1,51 @@
+const data = {
+    products: [
+        {
+            name: 'Dog',
+            slug: 'dog',
+            image: '/images/dog.jpg',
+            price: 150,
+            countInStock: 5,
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'kitten',
+            slug: 'kitten',
+            image: '/images/kitten.jpg',
+            price: 80,
+            countInStock: 10,
+            rating: 4.8,
+            numReviews: 10
+        },
+        {
+            name: 'Bird',
+            slug: 'bird',
+            image: '/images/bird.jpg',
+            price: 200,
+            countInStock: 3,
+            rating: 4.2,
+            numReviews: 10
+        },
+        {
+            name: 'Guinea Pig',
+            slug: 'guinea-pig',
+            image: '/images/guineapig.jpg',
+            price: 100,
+            countInStock: 5,
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Chicken',
+            slug: 'chicken',
+            image: '/images/chicken.jpg',
+            price: 300,
+            countInStock: 5,
+            rating: 4.9,
+            numReviews: 10
+        }
+    ]
+}
+
+export default data
